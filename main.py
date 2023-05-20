7aea2c7d8a8934ae6357f8dce0629d52c96c897a
PlainOldKevin <61211742+PlainOldKevin@users.noreply.github.com>
2023-05-20 00:35:27 -0400

    screen title and exit on click

---
+++ main.py
@@ -0,0 +1,11 @@
+# Imports
+from turtle import Turtle, Screen
+
+# Create screen and give characteristics
+screen = Screen()
+screen.setup(600, 600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+
+# Exit on click
+screen.exitonclick()
