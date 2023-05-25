a3dcb7b6fd749044cc53340a9e3cb04b945b52d5
unknown <kevinconwaylax@gmail.com>
2023-05-25 13:44:33 -0400

    changed scoreboard characteristics

--- scoreboard.py
+++ scoreboard.py
@@ -7,9 +7,10 @@ class Scoreboard(Turtle):
     def __init__(self) -> None:
         # Define attributes
         super().__init__()
-        self.goto(0, 290)
+        self.goto(0, 270)
         self.score = 0
-        self.write(f"Score: {self.score}", align="center", font=('Arial', 16, 'normal'))
+        self.color("white")
+        self.write(f"Score: {self.score}", align="center", font=('Arial', 18, 'normal'))
 
     # Function to increase score
     def increase_score(self):
