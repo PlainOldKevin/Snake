7af7cbc67a05a26f2b9d166451acaedfaabfa8c6
unknown <kevinconwaylax@gmail.com>
2023-05-25 13:40:57 -0400

    scoreboard init method complete

--- scoreboard.py
+++ scoreboard.py
@@ -1,2 +1,16 @@
 # Imports
 from turtle import Turtle
+
+class Scoreboard(Turtle):
+
+    # Init method
+    def __init__(self) -> None:
+        # Define attributes
+        super().__init__()
+        self.goto(0, 290)
+        self.score = 0
+        self.write(f"Score: {self.score}", align="center", font=('Arial', 16, 'normal'))
+
+    # Function to increase score
+    def increase_score(self):
+        self.score += 1
