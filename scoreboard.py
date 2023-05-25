b2b9596ee0b2333b3ca36dbc3493a6a8b3c014f1
unknown <kevinconwaylax@gmail.com>
2023-05-25 15:13:51 -0400

    fixed scoreboard font

--- scoreboard.py
+++ scoreboard.py
@@ -1,6 +1,9 @@
 # Imports
 from turtle import Turtle
 
+# Constants
+SCOREBOARD_TEXT = ('Courier', 18, 'normal')
+
 class Scoreboard(Turtle):
 
     # Init method
@@ -10,11 +13,11 @@ class Scoreboard(Turtle):
         self.goto(0, 270)
         self.score = 0
         self.color("white")
-        self.write(f"Score: {self.score}", align="center", font=('Arial', 18, 'normal'))
+        self.write(f"Score: {self.score}", align="center", font=SCOREBOARD_TEXT)
         self.hideturtle()
 
     # Function to increase score
     def increase_score(self):
         self.score += 1
         self.clear()
-        self.write(f"Score: {self.score}", align="center", font=('Arial', 18, 'normal'))
+        self.write(f"Score: {self.score}", align="center", font=SCOREBOARD_TEXT)
