3ced2b0b250e3eb223cea12edf506ead4980cc65
unknown <kevinconwaylax@gmail.com>
2023-05-30 11:26:45 -0400

    high score functionality completed

--- scoreboard.py
+++ scoreboard.py
@@ -12,7 +12,11 @@ class Scoreboard(Turtle):
         super().__init__()
         self.goto(0, 270)
         self.score = 0
-        self.highscore = 0
+        
+        # Open data file and define highscore
+        with open("data.txt") as data:
+            self.highscore = int(data.read())
+
         self.color("white")
         self.write(f"Score: {self.score} High Score: {self.highscore}", align="center", font=SCOREBOARD_TEXT)
         self.hideturtle()
@@ -26,6 +30,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.highscore:
             self.highscore = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.score}")
         self.score = 0
         self.update_score()
 
